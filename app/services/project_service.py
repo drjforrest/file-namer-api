5a49f1b44df224694eb06b128cef7f236f02cae1
Jamie Forrest <jforrest@purposeafrica.com>
2025-03-05 20:47:50 -0800

    Simplify project ID generation using dateutil for flexible date parsing

--- app/services/project_service.py
+++ app/services/project_service.py
@@ -1,35 +1,11 @@
 import datetime
+from dateutil import parser
 from app.models.project_model import create_project, get_db
 
 def generate_project_id(short_title, date_added):
     try:
-        # Try parsing with milliseconds and timezone offset with colon
-        try:
-            # First try with colon in timezone offset
-            date_obj = datetime.datetime.strptime(date_added, "%Y-%m-%dT%H:%M:%S.%f%z")
-        except ValueError:
-            try:
-                # Try without milliseconds but with timezone offset with colon
-                date_obj = datetime.datetime.strptime(date_added, "%Y-%m-%dT%H:%M:%S%z")
-            except ValueError:
-                try:
-                    # Try with milliseconds and Z suffix
-                    date_obj = datetime.datetime.strptime(date_added, "%Y-%m-%dT%H:%M:%S.%fZ")
-                except ValueError:
-                    try:
-                        # Try without milliseconds and with Z suffix
-                        date_obj = datetime.datetime.strptime(date_added, "%Y-%m-%dT%H:%M:%SZ")
-                    except ValueError:
-                        # Try with milliseconds and timezone offset without colon
-                        try:
-                            # Remove colon from timezone offset
-                            date_str = date_added[:-3] + date_added[-2:]
-                            date_obj = datetime.datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f%z")
-                        except ValueError:
-                            # Finally try without milliseconds and timezone offset without colon
-                            date_str = date_added[:-3] + date_added[-2:]
-                            date_obj = datetime.datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S%z")
-            
+        # Use dateutil.parser which is more flexible with date formats
+        date_obj = parser.parse(date_added)
         month, year = date_obj.strftime("%m"), date_obj.strftime("%Y")
 
         db = get_db()
@@ -42,5 +18,5 @@ def generate_project_id(short_title, date_added):
         create_project(project_id, month, year, seq)
 
         return project_id, None
-    except ValueError:
+    except (ValueError, TypeError):
         return None, "Invalid date format. Expected ISO format (e.g., YYYY-MM-DDTHH:MM:SSZ, YYYY-MM-DDTHH:MM:SS.fffZ, or YYYY-MM-DDTHH:MM:SS.fff±HH:MM)."
